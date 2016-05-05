38635f7ad0c07b0611b59aad1525c68b2c668692
Dan <danvolosnikov@gmail.com>
2016-05-05 02:01:12 +0000

    small changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,12 @@
 'use strict';
 
-var path = process.cwd();
+module.exports = function (app) {
 
-module.exports = function (app, passport) {
-
-	app.route('/')
-		.get( function (req, res) {
-			res.sendFile(path + '/public/index.html');
+	app.route('/').get(function (req, res) {
+			res.sendFile(process.cwd() + '/public/index.html');
 		});
+		
+	app.route('/new').get(function(req,res){
+		res.sendFile(process.cwd() + '/public/err.html'); // not elegant way , but I will figure that out later how to do it on jade engine.
+	});		
 };
--- server.js
+++ server.js
@@ -2,15 +2,32 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
+var mongo = require('mongodb');
+var api = require('./app/api/shortener.js');
 
 var app = express();
-require('dotenv').load();
+require('dotenv').load({silent:true});
+
+mongo.MongoClient.connect('mongodb://localhost:27017/shortener', function (err,db){
+
+if(err){
+    console.error('database failed to connect',err);
+    } else { console.log ('connected to mongodb on port 27017');}
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-routes(app);
+db.createCollection("sites",{
+    capped:true,
+    size:5242800,
+    max:5000
+});
+
+routes(app,db);
+api(app,db);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
 	console.log('Node.js listening on port ' + port + '...');
+});
+
 });
